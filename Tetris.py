a75277c97acdc61461b0d6aa29c31742ed2ccffe
CherryShen <cshen@wellesley.edu>
2016-07-30 15:53:07 -0700

    fixed bug in validate position logic so collision detection now works, commented out the keyboard up event but will leave it in there for now for example purposes

--- Tetris.py
+++ Tetris.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from pygame.locals import *
+from test import *
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -19,10 +20,11 @@ SHAPE_ARR = "shape_array"
 moving_left = False
 moving_right = False
 moving_down = False
-moving_up = False
+# moving_up = False
 
 def create_board():
     return [[0] * BOARD_WIDTH for _ in xrange(BOARD_HEIGHT)]
+
 
 def create_piece():
     return {"start_x": BOARD_WIDTH/2, "start_y": 1, SHAPE_ARR: [[0, 0, 1], [0, 1, 1], [0, 1, 0]]}
@@ -39,10 +41,12 @@ viewerSurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 block_row = len(current_block[SHAPE_ARR])
 block_col = len(current_block[SHAPE_ARR][0])
 
+
 def box_to_window_rect(x, y):
     x = BOX_LENGTH * x + BORDER_WIDTH
     y = BOX_LENGTH * y + BORDER_HEIGHT
     return x, y, BOX_LENGTH, BOX_LENGTH
+
 
 def draw_board(board):
     for i in xrange(BOARD_HEIGHT):
@@ -50,35 +54,41 @@ def draw_board(board):
             if board[i][j] != 0:
                 viewerSurface.fill(board[i][j], box_to_window_rect(j, i))
 
-def draw_piece(current_block):
-    for j in xrange(block_row):
-        for i in xrange(block_col):
-            if current_block[SHAPE_ARR][j][i] == 1:
-                viewerSurface.fill(GREEN, box_to_window_rect(current_block["start_x"]+i, current_block["start_y"] + j))
 
-def valid_position(current_block, x, y):
+def draw_piece(tetris_piece):
     for j in xrange(block_row):
         for i in xrange(block_col):
-            if current_block["start_x"] + i + x < -1 or current_block["start_x"] + i + x > BOARD_WIDTH - 1:
+            if tetris_piece[SHAPE_ARR][j][i] == 1:
+                viewerSurface.fill(GREEN, box_to_window_rect(tetris_piece["start_x"]+i, tetris_piece["start_y"] + j))
+
+
+def draw_block_on_board(tetris_piece, board):
+    for j in xrange(block_row):
+        for i in xrange(block_col):
+            if tetris_piece[SHAPE_ARR][j][i] != 0:
+                board[tetris_piece["start_y"]+j][tetris_piece["start_x"]+i] = RED
+
+
+def valid_position(tetris_piece, x, y):
+    for j in xrange(block_row):
+        for i in xrange(block_col):
+            next_x = tetris_piece["start_x"] + i + x
+            next_y = tetris_piece["start_y"] + j + y
+            if next_x < -1 or next_x > BOARD_WIDTH - 1:
                 return False
-            if current_block["start_y"] + j + y > BOARD_HEIGHT - 1:
+            if next_y > BOARD_HEIGHT - 1:
                 return False
-            if board[current_block["start_y"] + j][current_block["start_x"] + i] != 0:
+            if board[next_y][next_x] != 0 \
+                    and tetris_piece[SHAPE_ARR][j][i] == 1:
                 return False
     return True
 
-def draw_block_on_board(current_block, board):
-    for j in xrange(block_row):
-        for i in xrange(block_col):
-            if current_block[SHAPE_ARR][j][i] != 0:
-                board[current_block["start_y"]+j][current_block["start_x"]+i] = RED
 
-def check_block_collision(current_block):
+def check_block_collision(tetris_piece):
     for j in xrange(block_row):
         for i in xrange(block_col):
-            if j == block_row - 1:
-                if not valid_position(current_block, 0, 1):
-                    return True
+            if not valid_position(tetris_piece, 0, 1):
+                return True
     return False
 
 
@@ -91,8 +101,8 @@ while True:
         if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
                 moving_down = event.type == pygame.KEYDOWN
-            if event.key == pygame.K_UP:
-                moving_up = event.type == pygame.KEYDOWN
+            # if event.key == pygame.K_UP:
+            #     moving_up = event.type == pygame.KEYDOWN
             if event.key == pygame.K_LEFT:
                 moving_left = event.type == pygame.KEYDOWN
             if event.key == pygame.K_RIGHT:
@@ -110,10 +120,10 @@ while True:
         if moving_right:
             if valid_position(current_block, 1, 0):
                 current_block["start_x"] += 1
-    if current_y > 0:
-        if moving_up:
-            if valid_position(current_block, 0, -1):
-                current_block["start_y"] -= 1
+    # if current_y > 0:
+    #     if moving_up:
+    #         if valid_position(current_block, 0, -1):
+    #             current_block["start_y"] -= 1
 
     if check_block_collision(current_block):
         draw_block_on_board(current_block, board)
