1f15b99e5a305102524a16173e19396a69b75f22
CherryShen <cshen@wellesley.edu>
2016-07-30 12:13:55 -0700

    added a function to check for block collision - this will check if the block has reached the bottom of the board or if the block cannot go further as there will be another block in the way. i am still testing with the S-shape block for this purpose, will move the different shapes into code soon

--- Tetris.py
+++ Tetris.py
@@ -14,6 +14,8 @@ WINDOW_HEIGHT = BOARD_HEIGHT * BOX_LENGTH + BORDER_HEIGHT * 2
 RED = (255,   0,   0)
 GREEN = (0,   255,   0)
 
+SHAPE_ARR = "shape_array"
+
 moving_left = False
 moving_right = False
 moving_down = False
@@ -22,8 +24,8 @@ moving_up = False
 def create_board():
     return [[0] * BOARD_WIDTH for _ in xrange(BOARD_HEIGHT)]
 
-def create_piece(BOARD_WIDTH):
-     return {"start_x": BOARD_WIDTH/2, "start_y": 1, "shapeArr": [[0, 0, 1], [0, 1, 1], [0, 1, 0]]}
+def create_piece():
+    return {"start_x": BOARD_WIDTH/2, "start_y": 1, SHAPE_ARR: [[0, 0, 1], [0, 1, 1], [0, 1, 0]]}
 
 board = create_board()
 start_x = BOARD_WIDTH / 2
@@ -31,11 +33,11 @@ start_y = 1
 current_x = start_x
 current_y = start_y
 
-current_block = create_piece(BOARD_WIDTH)
+current_block = create_piece()
 viewerSurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-block_row = len(current_block["shapeArr"])
-block_col = len(current_block["shapeArr"][0])
+block_row = len(current_block[SHAPE_ARR])
+block_col = len(current_block[SHAPE_ARR][0])
 
 def box_to_window_rect(x, y):
     x = BOX_LENGTH * x + BORDER_WIDTH
@@ -49,29 +51,35 @@ def draw_board(board):
                 viewerSurface.fill(board[i][j], box_to_window_rect(j, i))
 
 def draw_piece(current_block):
-    for i in xrange(block_col):
-        for j in xrange(block_row):
-            if current_block["shapeArr"][j][i] == 1:
+    for j in xrange(block_row):
+        for i in xrange(block_col):
+            if current_block[SHAPE_ARR][j][i] == 1:
                 viewerSurface.fill(GREEN, box_to_window_rect(current_block["start_x"]+i, current_block["start_y"] + j))
 
 def valid_position(current_block, x, y):
-    for i in xrange(block_col):
-        for j in xrange(block_row):
+    for j in xrange(block_row):
+        for i in xrange(block_col):
             if current_block["start_x"] + i + x < -1 or current_block["start_x"] + i + x > BOARD_WIDTH - 1:
                 return False
             if current_block["start_y"] + j + y > BOARD_HEIGHT - 1:
                 return False
-            if board[i][j] != 0:
+            if board[current_block["start_y"] + j][current_block["start_x"] + i] != 0:
                 return False
     return True
 
 def draw_block_on_board(current_block, board):
-    for i in xrange(block_col):
-        for j in xrange(block_row):
-            if current_block["shapeArr"][i][j] != 0:
-                board[current_block["start_y"]+i][current_block["start_x"]+j] = RED
+    for j in xrange(block_row):
+        for i in xrange(block_col):
+            if current_block[SHAPE_ARR][j][i] != 0:
+                board[current_block["start_y"]+j][current_block["start_x"]+i] = RED
 
-#def check_block_collision(current_block):
+def check_block_collision(current_block):
+    for j in xrange(block_row):
+        for i in xrange(block_col):
+            if j == block_row - 1:
+                if not valid_position(current_block, 0, 1):
+                    return True
+    return False
 
 
 while True:
@@ -107,14 +115,12 @@ while True:
             if valid_position(current_block, 0, -1):
                 current_block["start_y"] -= 1
 
-    # if check_block_collision(current_block):
-    #     draw_block_on_board(current_block, board)
-    #     current_block = create_piece(BOARD_WIDTH)
+    if check_block_collision(current_block):
+        draw_block_on_board(current_block, board)
+        current_block = create_piece()
 
     viewerSurface.fill((0, 0, 0))
-    #viewerSurface.fill(GREEN, box_to_window_rect(current_x, current_y))
     draw_piece(current_block)
-    draw_block_on_board(current_block, board)
     draw_board(board)
 
     pygame.display.update()
