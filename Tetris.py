877ac40cdd4d5cf42aa9630073e91a7b7211bad8
CherryShen <cshen@wellesley.edu>
2016-07-30 11:14:43 -0700

    made the pieces show up on my board, only the S-shape piece is in the code as an example (I have the other variations stored in another file and need to decide how to put it on my next one). also made a function that draws the current block on the board. still a work in progress but adding some of my steps

--- Tetris.py
+++ Tetris.py
@@ -22,8 +22,8 @@ moving_up = False
 def create_board():
     return [[0] * BOARD_WIDTH for _ in xrange(BOARD_HEIGHT)]
 
-# def create_piece(x, y):
-#     return x, y, BOX_LENGTH, BOX_LENGTH
+def create_piece(BOARD_WIDTH):
+     return {"start_x": BOARD_WIDTH/2, "start_y": 1, "shapeArr": [[0, 0, 1], [0, 1, 1], [0, 1, 0]]}
 
 board = create_board()
 start_x = BOARD_WIDTH / 2
@@ -31,10 +31,11 @@ start_y = 1
 current_x = start_x
 current_y = start_y
 
-# currentPiece = create_piece(BOARD_WIDTH / 2, 1)
-
-current_block = current_x, current_y
+current_block = create_piece(BOARD_WIDTH)
 viewerSurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+block_row = len(current_block["shapeArr"])
+block_col = len(current_block["shapeArr"][0])
 
 def box_to_window_rect(x, y):
     x = BOX_LENGTH * x + BORDER_WIDTH
@@ -46,6 +47,32 @@ def draw_board(board):
         for j in xrange(BOARD_WIDTH):
             if board[i][j] != 0:
                 viewerSurface.fill(board[i][j], box_to_window_rect(j, i))
+
+def draw_piece(current_block):
+    for i in xrange(block_col):
+        for j in xrange(block_row):
+            if current_block["shapeArr"][j][i] == 1:
+                viewerSurface.fill(GREEN, box_to_window_rect(current_block["start_x"]+i, current_block["start_y"] + j))
+
+def valid_position(current_block, x, y):
+    for i in xrange(block_col):
+        for j in xrange(block_row):
+            if current_block["start_x"] + i + x < -1 or current_block["start_x"] + i + x > BOARD_WIDTH - 1:
+                return False
+            if current_block["start_y"] + j + y > BOARD_HEIGHT - 1:
+                return False
+            if board[i][j] != 0:
+                return False
+    return True
+
+def draw_block_on_board(current_block, board):
+    for i in xrange(block_col):
+        for j in xrange(block_row):
+            if current_block["shapeArr"][i][j] != 0:
+                board[current_block["start_y"]+i][current_block["start_x"]+j] = RED
+
+#def check_block_collision(current_block):
+
 
 while True:
     for event in pygame.event.get():
@@ -65,19 +92,29 @@ while True:
 
     if current_y < BOARD_HEIGHT - 1:
         if moving_down:
-            current_y += 1
+            if valid_position(current_block, 0, 1):
+                current_block["start_y"] += 1
     if current_x > 0:
         if moving_left:
-            current_x -= 1
+            if valid_position(current_block, -1, 0):
+                current_block["start_x"] -= 1
     if current_x < BOARD_WIDTH - 1:
         if moving_right:
-            current_x += 1
+            if valid_position(current_block, 1, 0):
+                current_block["start_x"] += 1
     if current_y > 0:
         if moving_up:
-            current_y -= 1
+            if valid_position(current_block, 0, -1):
+                current_block["start_y"] -= 1
+
+    # if check_block_collision(current_block):
+    #     draw_block_on_board(current_block, board)
+    #     current_block = create_piece(BOARD_WIDTH)
 
     viewerSurface.fill((0, 0, 0))
-    viewerSurface.fill(GREEN, box_to_window_rect(current_x, current_y))
+    #viewerSurface.fill(GREEN, box_to_window_rect(current_x, current_y))
+    draw_piece(current_block)
+    draw_block_on_board(current_block, board)
     draw_board(board)
 
     pygame.display.update()
