1a29f63030f5e13bb4b2376fe9944ca13c85411d
CherryShen <cshen@wellesley.edu>
2016-08-08 22:12:40 -0700

    added pause feature that pauses game when P is pressed

--- Tetris.py
+++ Tetris.py
@@ -7,7 +7,7 @@ from random import randint
 pygame.init()
 clock = pygame.time.Clock()
 
-game_over_font = pygame.font.Font(None, 45)
+game_font = pygame.font.Font(None, 45)
 
 moving_left = False
 moving_right = False
@@ -19,6 +19,8 @@ game_over = False
 score = 0
 level = 1
 cleared_lines = 0
+game_paused = False
+i=0
 
 def create_board():
     return [[0] * BOARD_WIDTH for _ in xrange(BOARD_HEIGHT)]
@@ -46,6 +48,7 @@ def rotate_block(tetris_piece):
     tetris_piece[PIECE_ROTATION] = new_rotation
 
 def level_and_score():
+    global cleared_lines
     score = cleared_lines
     level = cleared_lines % 10
     return score, level
@@ -55,6 +58,8 @@ board = create_board()
 current_block = create_piece()
 viewerSurface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption(TITLE)
+game_window_width = viewerSurface.get_width() / 2
+game_window_height = viewerSurface.get_height() / 2
 
 def box_to_window_rect(x, y):
     x = BOX_LENGTH * x + BORDER_WIDTH
@@ -129,6 +134,9 @@ def check_block_collision(tetris_piece):
                 return True
     return False
 
+pause_text = game_font.render(TEXT_GAME_PAUSED, True, BLACK)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -146,50 +154,53 @@ while True:
                 moving_right = event.type == pygame.KEYDOWN
             if event.key == pygame.K_SPACE:
                 is_fast_drop = event.type == pygame.KEYDOWN
+            if event.key == pygame.K_p and event.type == pygame.KEYDOWN:
+                game_paused = not game_paused
 
-    if is_fast_drop:
-        while valid_position(current_block, 0, 1):
-            current_block[PIECE_START_Y] += 1
-            last_falling_block_time = pygame.time.get_ticks()
+    if not game_paused:
+        if is_fast_drop:
+            while valid_position(current_block, 0, 1):
+                current_block[PIECE_START_Y] += 1
+                last_falling_block_time = pygame.time.get_ticks()
 
-    if current_block[PIECE_START_Y] < BOARD_HEIGHT - 1:
-        if moving_down:
+        if current_block[PIECE_START_Y] < BOARD_HEIGHT - 1:
+            if moving_down:
+                if valid_position(current_block, 0, 1):
+                    current_block[PIECE_START_Y] += 1
+                    last_falling_block_time = pygame.time.get_ticks()
+
+        if current_block[PIECE_START_X] >= 0:
+            if moving_left:
+                if valid_position(current_block, -1, 0):
+                    current_block[PIECE_START_X] -= 1
+                    last_falling_block_time = pygame.time.get_ticks()
+
+        if current_block[PIECE_START_X] < BOARD_WIDTH - 1:
+            if moving_right:
+                if valid_position(current_block, 1, 0):
+                    current_block[PIECE_START_X] += 1
+                    last_falling_block_time = pygame.time.get_ticks()
+
+        if pygame.time.get_ticks() - last_falling_block_time > FALLING_BLOCK_FREQUENCY:
             if valid_position(current_block, 0, 1):
                 current_block[PIECE_START_Y] += 1
                 last_falling_block_time = pygame.time.get_ticks()
 
-    if current_block[PIECE_START_X] >= 0:
-        if moving_left:
-            if valid_position(current_block, -1, 0):
-                current_block[PIECE_START_X] -= 1
-                last_falling_block_time = pygame.time.get_ticks()
+        if rotate:
+            rotated_block = create_piece()
+            for entry in current_block:
+                rotated_block[entry] = current_block[entry]
+            rotate_block(rotated_block)
+            if valid_position(rotated_block, 0, 0):
+                current_block = rotated_block
 
-    if current_block[PIECE_START_X] < BOARD_WIDTH - 1:
-        if moving_right:
-            if valid_position(current_block, 1, 0):
-                current_block[PIECE_START_X] += 1
-                last_falling_block_time = pygame.time.get_ticks()
-
-    if pygame.time.get_ticks() - last_falling_block_time > FALLING_BLOCK_FREQUENCY:
-        if valid_position(current_block, 0, 1):
-            current_block[PIECE_START_Y] += 1
-            last_falling_block_time = pygame.time.get_ticks()
-
-    if rotate:
-        rotated_block = create_piece()
-        for entry in current_block:
-            rotated_block[entry] = current_block[entry]
-        rotate_block(rotated_block)
-        if valid_position(rotated_block, 0, 0):
-            current_block = rotated_block
-
-    if check_block_collision(current_block):
-        if not valid_position(current_block, 0, 0):
-            game_over = True
-        else:
-            draw_block_on_board(current_block, board)
-            is_fast_drop = False
-            current_block = create_piece()
+        if check_block_collision(current_block):
+            if not valid_position(current_block, 0, 0):
+                game_over = True
+            else:
+                draw_block_on_board(current_block, board)
+                is_fast_drop = False
+                current_block = create_piece()
 
     viewerSurface.fill((0, 0, 0))
 
@@ -198,11 +209,16 @@ while True:
     clear_lines()
     draw_board(board)
 
+    if game_paused:
+        viewerSurface.fill(WHITE)
+        viewerSurface.blit(pause_text, [game_window_width - pause_text.get_rect().width / 2, game_window_height - \
+                                        pause_text.get_rect().height / 2])
+
     if game_over:
-        game_over_text = game_over_font.render(TEXT_GAME_OVER, True, WHITE)
+        game_over_text = game_font.render(TEXT_GAME_OVER, True, WHITE)
         game_over_rect = game_over_text.get_rect()
-        game_over_x = viewerSurface.get_width() / 2 - game_over_rect.width / 2
-        game_over_y = viewerSurface.get_height() / 2 - game_over_rect.height / 2
+        game_over_x = game_window_width - game_over_rect.width / 2
+        game_over_y = game_window_height - game_over_rect.height / 2
         viewerSurface.blit(game_over_text, [game_over_x, game_over_y])
 
     pygame.display.update()
